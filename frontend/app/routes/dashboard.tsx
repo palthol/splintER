a0daa8df1448f1c7b11498fc5fb40c8f62aa000a
palthol <ilikericeat@gmail.com>
2025-03-03 22:26:42 -0500

    feature:
    added pages for the routes.

---
+++ frontend/app/routes/dashboard.tsx
@@ -0,0 +1,29 @@
+import LandingPage from "~/components/Hero";
+import AnimatedWaveBackground from "~/components/AnimatedWaveBackground";
+import type { Route } from "./+types/home";
+import Navbar from "~/components/navbar";
+import Footer from "~/components/footer";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Splinter - League Dashboard" },
+    { name: "description", content: "Welcome to Splinter, your personalized League of Legends dashboard." },
+  ];
+}
+
+export default function Dashboard() {
+    return (
+        <div className="flex flex-col min-h-screen relative">
+          <Navbar />
+          <main className="relative flex-grow">
+          
+            <div className="relative z-10">
+             
+            </div>
+          </main>
+          <footer>
+            <Footer />
+          </footer>
+        </div>
+      );
+    }
---
+++ frontend/app/routes/profile.tsx
@@ -0,0 +1,28 @@
+import type { Route } from "./+types/home";
+import Navbar from "~/components/navbar";
+import Footer from "~/components/footer";
+import AuthForm from "~/components/AuthForm";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Splinter - League Dashboard" },
+    { name: "description", content: "Welcome to Splinter, your personalized League of Legends dashboard." },
+  ];
+}
+
+export default function Profile() {
+  return (
+    <div className="flex flex-col min-h-screen relative">
+      <Navbar />
+      <main className="relative flex-grow">
+      
+        <div className="relative z-10">
+         <AuthForm />
+        </div>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+}

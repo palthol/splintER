e6b2fd901c0038512ebcd7817e777d4b85795338
palthol <ilikericeat@gmail.com>
2025-03-03 19:14:26 -0500

    Inital commit, added react router v7

---
+++ frontend/app/routes.ts
@@ -0,0 +1,10 @@
+import { type RouteConfig, index } from "@react-router/dev/routes";
+
+export default 
+[index("routes/home.tsx"),
+//  route("/test", "routes/test1.tsx"),
+//  route("/test2", "routes/test2.tsx"),
+
+
+
+] satisfies RouteConfig;
---
+++ frontend/app/routes/home.tsx
@@ -0,0 +1,14 @@
+import LandingPage from "~/components/LandingPage";
+import type { Route } from "./+types/home";
+
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return<LandingPage/>;
+}
